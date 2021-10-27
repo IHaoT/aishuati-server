71fa384f21724fd484e67a41daf6c66afcd38b87
fy-j <1289757736@qq.com>
2021-10-27 16:03:38 +0800

    项目创建
    springboot项目创建

---
+++ src/main/java/com/example/aishuatiserver/AishuatiServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.aishuatiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AishuatiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AishuatiServerApplication.class, args);
+    }
+
+}
