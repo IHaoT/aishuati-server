a952db07bd2e3f31fca2baf39623bc636556187b
fy-j <1289757736@qq.com>
2021-11-30 13:45:30 +0800

    Create SpringSessionConfig.java

---
+++ src/main/java/com/example/aishuatiserver/config/SpringSessionConfig.java
@@ -0,0 +1,21 @@
+package com.example.aishuatiserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
+
+@Configuration
+public class SpringSessionConfig {
+    public SpringSessionConfig() {
+    }
+
+    @Bean
+    public CookieSerializer httpSessionIdResolver() {
+        DefaultCookieSerializer cookieSerializer = new DefaultCookieSerializer();
+        // 取消仅限同一站点设置
+        cookieSerializer.setSameSite("None");
+        cookieSerializer.setUseSecureCookie(true);
+        return cookieSerializer;
+    }
+}
